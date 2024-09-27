4ba46f4067158ff73a46b5bf9c10e3c71f160453
Yugesh-0831 <yugeshjhamb0831@gmail.com>
2024-09-27 19:02:48 +0530

    updated backend

--- SocialMediaApp/index.js
+++ SocialMediaApp/index.js
@@ -8,60 +8,60 @@ const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
 
-
 dotenv.config();
 
-async function main(){
-try{
-await mongoose.connect(process.env.MONGO_URL);
-console.log("connected");
-}
-catch(e){
+async function main() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("connected");
+  } catch (e) {
     console.log(e.message);
-}
+  }
 }
 
 main();
 
-
-const userRoute = require("./routes/users")
-const authRoute = require("./routes/auth")
-const postRoute = require("./routes/posts")
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
 //middleware
-app.use("/images", express.static(path.join(__dirname,"public/images")))
+app.use(express.static(path.resolve(__dirname, "build")));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.json()); //it lets our server accept json
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
-app.use(cors({
-    origin: '*'
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "public/images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
-  
-  const upload = multer({ storage: storage });
-  app.post("/api/upload", upload.single("file"), (req, res) => {
-    try {
-      return res.status(200).json("File uploded successfully");
-    } catch (error) {
-      console.error(error);
-      res.json(e.message);
-    }
-  });
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-app.use("/api/users" , userRoute);
-app.use("/api/auth" , authRoute);
-app.use("/api/posts" , postRoute);
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+    res.json(e.message);
+  }
+});
 
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
-app.listen(8800 , ()=>{
-    console.log("backend server is readyy");
-
-})
-
+app.listen(8800, () => {
+  console.log("backend server is readyy");
+});
